9845b3fee3e5fabc2c71a2fdfab6564d12ce7a2e
alikamal234-gif <alikamal0675@gmail.com>
2026-05-23 11:57:34 +0100

    fix probleme de login et travaille de register

--- src/controllers/authentification.js
+++ src/controllers/authentification.js
@@ -63,11 +63,16 @@ export const login = async (req, res) => {
         );
 
         res.cookie("token", token, {
-            httpOnly: true,
-            secure: false, 
-            sameSite: "strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-        });
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge:
+      7 *
+      24 *
+      60 *
+      60 *
+      1000,
+});
 
         res.json({
             success: true,
