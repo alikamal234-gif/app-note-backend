33ab4c300994eeddd11aa51bdb5c24ad810ae7f6
alikamal234-gif <alikamal0675@gmail.com>
2026-05-23 11:42:43 +0100

    fix router

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,9 +3,7 @@ import { login, register,getUsers } from "../controllers/authentification.js";
 const router = express.Router();
 
 
-router.post('/register' , (req,res) => {
-  console.log("khdam")
-});
+router.post('/register' , register);
 router.post('/login' , login);
 router.get('/users' , getUsers);
 
