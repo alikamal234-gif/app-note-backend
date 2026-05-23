d06e2ac5a443168a9847bf5c174f3cbee0be91a0
alikamal234-gif <alikamal0675@gmail.com>
2026-05-23 12:11:16 +0100

    test token

--- src/controllers/authentification.js
+++ src/controllers/authentification.js
@@ -62,17 +62,18 @@ export const login = async (req, res) => {
             }
         );
 
-        res.cookie("token", token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
-    maxAge:
-      7 *
-      24 *
-      60 *
-      60 *
-      1000,
-});
+        res.cookie("token",token,{
+                httpOnly: true,
+                secure: false,
+                sameSite: "lax",
+                maxAge:
+                  7 *
+                  24 *
+                  60 *
+                  60 *
+                  1000,
+            }
+        );
 
         res.json({
             success: true,
